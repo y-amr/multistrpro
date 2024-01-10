b98e7c74c170fd98a36f03a91d58e62615a76f8b
YANIS <yanis.amraoui@rechargermonauto.com>
2024-01-10 02:45:34 +0100

    Update test + add stream.json

--- test.py
+++ test.py
@@ -1,13 +1,36 @@
 import subprocess
 import time
 import threading
+import json
+import os
+import random
+from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 streams = {}  # Dictionnaire pour stocker les streams actifs (stream_id -> process)
 @app.route('/startstream', methods=['POST'])
 def start_stream():
+    #test
+    # Chemin du dossier contenant les vidéos
+    video_folder = '/root/flask_app/movie'
+
+    # Lister tous les fichiers dans le dossier
+    video_files = os.listdir(video_folder)
+
+    # Filtrer la liste pour ne garder que les fichiers vidéo (optionnel, selon vos besoins)
+    # Par exemple, pour ne garder que les fichiers .mp4 :
+    video_files = [file for file in video_files if file.endswith('.mp4')]
+
+    # Sélectionner aléatoirement un fichier vidéo
+    selected_video = random.choice(video_files)
+
+    # Chemin complet de la vidéo sélectionnée
+    video_path = os.path.join(video_folder, selected_video)
+
+
     data = request.json
     stream_id = data.get('stream_id')
     stream_duration = data.get('stream_duration')  # Nouveau paramètre
@@ -16,33 +39,76 @@ def start_stream():
         # Vérifier si le stream est déjà en cours
         if stream_id in streams and not streams[stream_id].poll():
             return jsonify({'message': 'Stream already active', 'status': 'active'})
-
-        # Démarrer le stream avec ffmpeg
-        process = subprocess.Popen(f'ffmpeg -stream_loop -1 -re -i /root/flask_app/movie.mp4 -stream_loop -1 -i /root/flask_app/music.mp3 -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv "rtmp://live.twitch.tv/app/{stream_id}"', shell=True)
+        print(video_path)
+        # Modifier la commande ffmpeg pour utiliser la vidéo sélectionnée
+        process = subprocess.Popen(f'ffmpeg -stream_loop -1 -re -i {video_path} -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv "rtmp://live.twitch.tv/app/{stream_id}"', shell=True)
         streams[stream_id] = process  # Stocker le processus associé au stream_id
 
         # Lancer un thread pour arrêter le stream après la durée spécifiée
         threading.Thread(target=stop_after_duration, args=(stream_id, stream_duration)).start()
 
+        save_stream_info(stream_id, process.pid, stream_duration)
+
         return jsonify({'message': 'Stream started', 'status': 'active'})
     else:
         return jsonify({'error': 'Stream ID or duration not provided'}), 400
 
-# Fonction pour arrêter le stream après une durée spécifiée
-def stop_after_duration(stream_id, duration):
-    time.sleep(duration * 60)  # Convertir la durée en minutes en secondes
-    if stream_id in streams and not streams[stream_id].poll():
-        streams[stream_id].terminate()
-        del streams[stream_id]
+def save_stream_info(stream_id, process_id, duration):
+    end_time = datetime.now() + timedelta(minutes=duration)
+    stream_info = {
+        'process_id': process_id,
+        'end_time': end_time.strftime('%Y-%m-%d %H:%M:%S')
+    }
+    with open('streams.json', 'r+') as file:
+        streams = json.load(file)
+        streams[stream_id] = stream_info
+        file.seek(0)
+        json.dump(streams, file)
 
-@app.route('/stopallstream', methods=['GET'])
+def stop_after_duration(stream_id, duration):
+    time.sleep(duration * 60)
+    with open('streams.json', 'r+') as file:
+        streams = json.load(file)
+        if stream_id in streams:
+            process_id = streams[stream_id]['process_id']
+            subprocess.Popen(f'kill {process_id}', shell=True)
+            del streams[stream_id]
+            file.seek(0)
+            file.truncate()
+            json.dump(streams, file)
+
+
+@app.route('/stopallstream', methods=['POST'])
 def stop_all_streams():
-    for stream_id in streams:
-        if not streams[stream_id].poll():
-            streams[stream_id].terminate()
-    streams.clear()  # Effacer tous les streams de la liste
+    with open('streams.json', 'r+') as file:
+        streams = json.load(file)
+        for stream_id, info in streams.items():
+            subprocess.Popen(f'kill {info["process_id"]}', shell=True)
+        file.seek(0)
+        file.truncate()
+        json.dump({}, file)
     return jsonify({'message': 'All streams stopped'}), 200
+
+def check_streams():
+    with open('streams.json', 'r+') as file:
+        streams = json.load(file)
+        current_time = datetime.now()
+        for stream_id, info in list(streams.items()):
+            end_time = datetime.strptime(info['end_time'], '%Y-%m-%d %H:%M:%S')
+            if current_time >= end_time:
+                subprocess.Popen(f'kill {info["process_id"]}', shell=True)
+                del streams[stream_id]
+
+        file.seek(0)
+        file.truncate()
+        json.dump(streams, file)
+        print('Checked streams')
+        # Réinitialiser le timer pour exécuter à nouveau cette fonction après 30 secondes
+        threading.Timer(30, check_streams).start()
 
 
 if __name__ == '__main__':
+    # Démarrer la vérification périodique des streams
+    check_streams()
+
     app.run(host='0.0.0.0', port=6000)
