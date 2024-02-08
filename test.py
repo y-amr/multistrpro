2306328a7cd96c01eebfd1b50ba490c3e943e213
Yanis <71019123+y-amr@users.noreply.github.com>
2024-02-09 00:07:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
+path1 = '/root/flask_app/'
+path= ''
 def find_ffmpeg_pid(stream_key):
     try:
         # Utiliser la commande ps pour obtenir la liste des processus
@@ -28,47 +29,6 @@ def find_ffmpeg_pid(stream_key):
     except Exception as e:
         print(f"Erreur lors de la recherche du PID de ffmpeg : {e}")
         return None
-
-@app.route('/startstream', methods=['POST'])
-def start_stream():
-
-    # Chemin du dossier contenant les vidéos
-    video_folder = '/root/flask_app/movie'
-
-    # Lister tous les fichiers dans le dossier
-    video_files = os.listdir(video_folder)
-
-    # Filtrer la liste pour ne garder que les fichiers vidéo (optionnel, selon vos besoins)
-    # Par exemple, pour ne garder que les fichiers .mp4 :
-    video_files = [file for file in video_files if file.endswith('.mp4')]
-
-    # Sélectionner aléatoirement un fichier vidéo
-    selected_video = random.choice(video_files)
-
-    # Chemin complet de la vidéo sélectionnée
-    video_path = os.path.join(video_folder, selected_video)
-
-    data = request.json
-    stream_id = data.get('stream_id')
-    stream_duration = data.get('stream_duration')  # Nouveau paramètre
-
-    if stream_id and stream_duration:
-        # Vérifier si le stream est déjà en cours
-        if stream_id in streams and not streams[stream_id]['process'].poll():
-            return jsonify({'message': 'Stream already active', 'status': 'active'})
-
-        # Modifier la commande ffmpeg pour utiliser la vidéo sélectionnée
-        process = subprocess.Popen(f'ffmpeg -stream_loop -1 -re -i {video_path} -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv "rtmp://live.twitch.tv/app/{stream_id}"', shell=True)
-        
-        streams[stream_id] = {'process': process, 'start_time': time.time()}  # Stocker le processus associé au stream_id et l'heure de début
-
-        # Planifier l'arrêt du stream après la durée spécifiée
-        threading.Thread(target=stop_stream, args=(stream_id, stream_duration)).start()
-
-        return jsonify({'message': 'Stream started', 'status': 'active'})
-    else:
-        return jsonify({'error': 'Stream ID or duration not provided'}), 400
-
 
 def save_stream_info(stream_id, process_id, duration):
     end_time = datetime.now() + timedelta(minutes=duration)
@@ -109,7 +69,6 @@ def stop_all_streams():
         app.logger.error(f'Error stopping ffmpeg streams: {e}')
         return jsonify({'error': 'Failed to stop ffmpeg streams'}), 500
 
-
 def check_streams():
     with open('/root/flask_app/streams.json', 'r+') as file:
         streams = json.load(file)
@@ -136,9 +95,53 @@ def check_streams():
         # Réinitialiser le timer pour exécuter à nouveau cette fonction après 30 secondes
         threading.Timer(30, check_streams).start()
 
+streams = {}
+
+@app.route('/startstream', methods=['POST'])
+def start_stream():
+
+    # Chemin du dossier contenant les vidéos
+    video_folder = path + 'movie'
+
+    # Lister tous les fichiers dans le dossier
+    video_files = os.listdir(video_folder)
+
+    # Filtrer la liste pour ne garder que les fichiers vidéo (optionnel, selon vos besoins)
+    # Par exemple, pour ne garder que les fichiers .mp4 :
+    video_files = [file for file in video_files if file.endswith('.mp4')]
+
+    # Sélectionner aléatoirement un fichier vidéo
+    selected_video = random.choice(video_files)
+
+    # Chemin complet de la vidéo sélectionnée
+    video_path = os.path.join(video_folder, selected_video)
+
+    data = request.json
+    stream_id = data.get('stream_id')
+    stream_duration = data.get('stream_duration')  # Nouveau paramètre
+
+    if stream_id and stream_duration:
+        # Vérifier si le stream est déjà en cours
+        if stream_id in streams and not streams[stream_id]['process'].poll():
+            return jsonify({'message': 'Stream already active', 'status': 'active'})
+
+        # Modifier la commande ffmpeg pour utiliser la vidéo sélectionnée
+        process = subprocess.Popen(f'ffmpeg -stream_loop -1 -re -i {video_path} -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv "rtmp://live.twitch.tv/app/{stream_id}"', shell=True)
+        
+        streams[stream_id] = {'process': process, 'start_time': time.time(), 'duration': stream_duration}  # Stocker le processus associé au stream_id, l'heure de début et la durée
+
+        # Planifier l'arrêt du stream après la durée spécifiée
+        threading.Thread(target=stop_stream, args=(stream_id, stream_duration)).start()
+
+        return jsonify({'message': 'Stream started', 'status': 'active'})
+    else:
+        return jsonify({'error': 'Stream ID or duration not provided'}), 400
+
 # Fonction pour surveiller l'état du stream
 def check_stream_state():
     while True:
+        print( streams)
+        print("Checking stream state")
         for stream_id, stream_info in streams.items():
             process = stream_info['process']
             remaining_time = stream_info['start_time'] + stream_info['duration'] - time.time()
@@ -147,10 +150,11 @@ def check_stream_state():
                 print(f"Stream {stream_id} has finished.")
             else:
                 print(f"Stream {stream_id} - Remaining time: {remaining_time} seconds")
-        time.sleep(10)  # Vérifier toutes les 60 secondes
+        time.sleep(4)  # Vérifier toutes les 60 secondes
 
 if __name__ == '__main__':
-    # Démarrer la vérification périodique des streams
-    check_stream_state()
+    stream_thread = threading.Thread(target=check_stream_state)
+    stream_thread.daemon = True  # Terminer le thread lorsque le programme principal se termine
+    stream_thread.start()
 
     app.run(host='0.0.0.0', port=6000)
